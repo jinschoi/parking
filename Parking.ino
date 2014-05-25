2e497982dae588f608bfbc4c02f8bd0f9d9905fc
Jin Choi <jsc@alum.mit.edu>
2014-05-24 22:50:43 -0400

    Distinguish between idle states of "parked car" and "no car".

--- Parking.ino
+++ Parking.ino
@@ -2,13 +2,18 @@
 #include <JeeLib.h>
 #include <avr/interrupt.h>
 
-// Pins. Also used as state machine states.
+// Pins.
 #define RED A2
 #define YELLOW A1
 #define GREEN A0
 
-// Not a pin, just a state.
-#define SLEEP 0
+// States
+// Car is not present.
+#define WAITING 0
+// Car is present and moving.
+#define PARKING 1
+// Car is present and not moving.
+#define PARKED 2
 
 // Pins for SR04.
 #define TRIGGER_PIN 8
@@ -31,8 +36,11 @@
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DIST);
 uint16_t ticks = 0xffff;
 int state;
+int range;
 int last_distance;
 int idle_count;
+int no_detect_count;
+int cm;
 
 void setup() {
 
@@ -48,9 +56,12 @@ void setup() {
   TCCR1A = B00000000;
   TCCR1B = B00001100; // CTC mode, 256 prescaler, for a max period of ~2s at 8 Mhz.
 
-  state = SLEEP;
+  state = WAITING;
+  range = 0;
   last_distance = MAX_DIST;
   idle_count = 0;
+  no_detect_count = 0;
+  cm = 0;
 }
 
 // Calculate a flash rate based on the given distance. Only called for yellow state.
@@ -58,18 +69,9 @@ inline uint16_t ticks_for_dist(int cm) {
   return FASTEST_FLASH_RATE + (SLOWEST_FLASH_RATE - FASTEST_FLASH_RATE) * (cm - YELLOW_DIST) / (GREEN_DIST - YELLOW_DIST);
 }
 
-void loop() {
-  if (state == SLEEP) {
-#ifdef DEBUG
-    delay(2000);
-#else
-    Sleepy::loseSomeTime(2000);
-#endif
-  }
-  else delay(100);
-
+int ping() {
   int uS = sonar.ping_median(3);
-  int cm = sonar.convert_cm(uS);
+  cm = sonar.convert_cm(uS);
 
 #ifdef DEBUG
   Serial.print(cm);
@@ -81,58 +83,122 @@ void loop() {
   Serial.println(idle_count);
 #endif
 
-  // Go to power saving if we haven't had any interesting events in a while.
-  if (idle_count > 50 && state != SLEEP) { // 5s if not already in sleep mode
-    state = SLEEP;
-    digitalWrite(RED, LOW);
-    stop_blinking();
-    digitalWrite(GREEN, LOW);
-    return;
-  }
+  return cm;
+}
 
-  // Ignore motion away, with a little slop factor thrown in.
-  if (cm >= last_distance - 5) {
-    idle_count++;
-    last_distance = max(last_distance, cm);
-    return;
-  }
-
-  // Ignore null detects.
-  if (cm == 0) {
-    idle_count++;
-    return;
-  }
-
-  last_distance = cm;
-  idle_count = 0;
-
-  if (cm >= GREEN_DIST) { // GREEN
-    if (state == YELLOW) stop_blinking();
-    if (state == RED) digitalWrite(RED, LOW);
-    if (state != GREEN) {
-      state = GREEN;
-      digitalWrite(GREEN, HIGH);
-    }
-  } else if (cm < YELLOW_DIST) { // RED
-    if (state == YELLOW) stop_blinking();
-    if (state == GREEN) digitalWrite(GREEN, LOW);
-    if (state != RED) {
-      state = RED;
-      digitalWrite(RED, HIGH);
-    }
-  } else { // YELLOW
-    if (state == GREEN) digitalWrite(GREEN, LOW);
-    if (state == RED) digitalWrite(RED, LOW);
-
-    ticks = ticks_for_dist(cm);
+void loop() {
+  if (state == WAITING) {
 #ifdef DEBUG
-    Serial.println("ticks: ");
-    Serial.println(ticks);
+    delay(2000);
+#else
+    Sleepy::loseSomeTime(2000);
+#endif
+
+    ping();
+    if (cm == 0) return; // Keep waiting.
+
+    // Got a return.
+    state = PARKING;
+    return;
+  }
+
+  if (state == PARKED) {
+#ifdef DEBUG
+    delay(60000);
+#else
+    Sleepy::loseSomeTime(60000);
+#endif
+
+    // If it's been 5 minutes since we've detected a car, go to the waiting state.
+    if (no_detect_count > 5) {
+      state = WAITING;
+      no_detect_count = 0;
+      return;
+    }
+
+    ping();
+    if (cm == 0) {
+      no_detect_count++;
+      return;
+    }
+    no_detect_count = 0;
+
+    if (cm < last_distance - 5 || cm > last_distance + 5) {
+      // Car not where we last saw it, back to parking state.
+      state = PARKING;
+    }
+
+    last_distance = cm;
+  }
+
+  if (state == PARKING) {
+    delay(100);
+
+    // If we have not detected a car in 5s, go back to the waiting for car state.
+    if (no_detect_count > 50) {
+      state = WAITING;
+      no_detect_count = 0;
+      idle_count = 0;
+      digitalWrite(RED, LOW);
+      stop_blinking();
+      digitalWrite(GREEN, LOW);
+      return;
+    }
+
+    if (idle_count > 300) { // 30s of no forward events; we're parked.
+      state = PARKED;
+      no_detect_count = 0;
+      idle_count = 0;
+      digitalWrite(RED, LOW);
+      stop_blinking();
+      digitalWrite(GREEN, LOW);
+      return;
+    }
+
+    ping();
+
+    if (cm == 0) {
+      no_detect_count++;
+      return;
+    }
+    no_detect_count = 0;
+
+    // Only take into account forward motion for purposes of idle detection.
+    if (cm >= last_distance - 5) { // 5 cm slop factor.
+      last_distance = max(last_distance, cm);
+      idle_count++;
+    } else {
+      last_distance = cm;
+      idle_count = 0;
+    }
+
+    if (cm >= GREEN_DIST) { // GREEN
+      if (range == YELLOW) stop_blinking();
+      if (range == RED) digitalWrite(RED, LOW);
+      if (range != GREEN) {
+        range = GREEN;
+        digitalWrite(GREEN, HIGH);
+      }
+    } else if (cm < YELLOW_DIST) { // RED
+      if (range == YELLOW) stop_blinking();
+      if (range == GREEN) digitalWrite(GREEN, LOW);
+      if (range != RED) {
+        range = RED;
+        digitalWrite(RED, HIGH);
+      }
+    } else { // YELLOW
+      if (range == GREEN) digitalWrite(GREEN, LOW);
+      if (range == RED) digitalWrite(RED, LOW);
+      ticks = ticks_for_dist(cm);
+#ifdef DEBUG
+      Serial.println("ticks: ");
+      Serial.println(ticks);
 #endif    
 
-    if (state != YELLOW) {
-      state = YELLOW;
-      start_blinking();
+      if (range != YELLOW) {
+        range = YELLOW;
+        start_blinking();
+      }
     }
   }
 }
